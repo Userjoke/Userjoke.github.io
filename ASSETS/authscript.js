476c64222324b906c7588a83675af128b862db30
Userjoke <118853449+Userjoke@users.noreply.github.com>
2022-11-26 23:33:44 +0100

    added janča functionality
    also cleaned up the rainbow mode script

---
+++ ASSETS/authscript.js
@@ -0,0 +1,16 @@
+let notapass = 0
+let input = 0
+
+function loadfnc(){
+notapass = Math.floor(Math.random()*1000000);
+}
+
+function submit() {
+  input = document.getElementById("inputpass").value
+  if(input == notapass){
+    window.location.href = "JS creations/secret/gallery.html"
+  } else {
+    document.getElementById("alerttext").style.color = "red"
+    setTimeout(function(){document.getElementById("alerttext").style.color = "#2f3136"}, 1200)
+  }
+}
